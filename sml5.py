1956001207eb879755ce7343a94f7472ae3ca011
Himang Garg <130616649+himangg@users.noreply.github.com>
2024-04-16 17:39:01 +0530

    Update sml5.py

--- sml5.py
+++ sml5.py
@@ -15,7 +15,6 @@ x_test_recon=[]
 y_train_recon=[]
 x_val_recon=[]
 y_val_recon=[]
-weights=[]
 
 count1=0
 count0=0
@@ -52,10 +51,9 @@ y_test=np.array(y_test_recon)
 x_val=np.array(x_val_recon)
 y_val=np.array(y_val_recon)
 
-print(f'X_train_reshaped shape: {x_train.shape}')
+# print(f'X_train_reshaped shape: {x_train.shape}')
 X=x_train
 X=X.T   
-
 mean=np.mean(X,axis=1 , keepdims=True)  
 X=X-mean   
 S=np.dot(X,X.T)/(X.shape[1]-1)  
@@ -69,8 +67,8 @@ Up = U[:, :p]
 Y = np.dot(Up.T,X)
 x_val = np.dot(U[:,  :p].T, (x_val.T-mean))
 x_test = np.dot(U[:,  :p].T, (x_test.T-mean))
-print(Y.shape)
-print(x_val.shape)
+# print(Y.shape)
+# print(x_val.shape)
 
 class Node:
     def __init__(self,feature_index=None,threshold=None,left=None,right=None,value=None,weight=None):
@@ -81,45 +79,31 @@ class Node:
         self.value=value
         self.weight=weight
 
-def decision_stump(stumpNo,Y,y_train,x_val,y_val,MSEs,h,h1):
+def decision_stump(stumpNo,Y,y_train,x_val,y_val,MSEs,h,h1,residues):
     mn=1000000000
     for i in range(5):
         unique_values=[]
         for j in range(y_train.size):
-            unique_values.append(Y[i][j])
+            unique_values.append([Y[i][j],y_train[j]])
         unique_values=np.array(unique_values)
-        unique_values.sort(axis=0)
+        unique_values=unique_values[np.argsort(unique_values[:, 0])]
+
         case_indexes=[]
         for j in range(1000):
             case_indexes.append(random.randint(0,y_train.size-2))
-        for j in case_indexes:
-            split=(unique_values[j]+unique_values[j+1])/2
-            left_sum=0
-            left_total=0
-            right_sum=0
-            right_total=0
-            for k in range(y_train.size):
-                if(Y[i][k]<=split):
-                    left_sum+=y_train[k]
-                    left_total+=1
-                else:
-                    right_sum+=y_train[k]
-                    right_total+=1
-                    
-            left_mean=left_sum/left_total
-            right_mean=right_sum/right_total
-            # print(left_sum,left_total)
-            # print(right_sum,right_total)
-            # print(mn)
-                
-            SSR=0
-            for k in range(y_train.size):
-                if(Y[i][k]<=split):
-                    SSR+=(y_train[k]-left_mean)**2
-                else:
-                    SSR+=(y_train[k]-right_mean)**2
 
-            # print(SSR)
+        for j in case_indexes:
+            split=((unique_values[j][0]+unique_values[j+1][0])/2).real
+
+            left_region=unique_values[:j+1]
+            right_region=unique_values[j+1:]
+            
+            left_mean=np.mean(left_region, axis = 0)[1].real
+            right_mean=np.mean(right_region,axis=0)[1].real
+                
+            SSR_left = np.sum((y_train[Y[i] <= split] - left_mean) ** 2)
+            SSR_right = np.sum((y_train[Y[i] > split] - right_mean) ** 2)
+            SSR = SSR_left + SSR_right
             if(SSR<mn):
                 mn=SSR
                 ans=[i,split,left_mean,right_mean]
@@ -134,17 +118,21 @@ def decision_stump(stumpNo,Y,y_train,x_val,y_val,MSEs,h,h1):
     
     h.append(temp)
 
-
-    residues=[]
+    # residues=[]
+    temp=[]
     for i in range(y_train.size):
-        residues.append(y_train[i])
+        if(stumpNo==0):
+            temp.append(y_train[i])
+        else:
+            temp.append(residues[stumpNo-1][i])
     
-    for i in range(stumpNo+1):
-        for j in range(y_train.size):
-            residues[j]-=(0.01)*h[i][j]
+    # for i in range(stumpNo+1):
+    for j in range(y_train.size):
+        temp[j]-=(0.01)*h[stumpNo][j]
     
+    residues.append(temp)
     for i in range(y_train.size):
-        y_train[i]=np.sign(residues[i])
+        y_train[i]=np.sign(residues[stumpNo][i])
 
 
     temp=[]
@@ -163,52 +151,58 @@ def decision_stump(stumpNo,Y,y_train,x_val,y_val,MSEs,h,h1):
             temp-=(0.01)*h1[i][j]
         MSE+=temp**2
 
-    # for i in range(y_val.size):
-    #     MSE+=(y_val[i]-(0.01)*temp[i])**2
-
     MSE/=y_val.size
-    print(MSE)
+    print("MSE for tree",stumpNo+1,":",MSE)
 
+    MSEs.append(MSE)
     return ans
-
-
-
-
 
 h=[]
 h1=[]
 MSEs=[]
 stumps=[]
+residues=[]
 
 noOfStumps=300
 for i in range(noOfStumps):
-    stumps.append(decision_stump(i,Y,y_train=y_train,x_val=x_val,y_val=y_val,MSEs=MSEs,h=h,h1=h1))
+    stumps.append(decision_stump(i,Y,y_train=y_train,x_val=x_val,y_val=y_val,MSEs=MSEs,h=h,h1=h1,residues=residues))
 
-print(MSEs)
+# print(MSEs)
 
-# minMSE=0
-# for i in range(noOfStumps):
-#     if(minMSE<MSEs[i]):
-#         minMSE=MSEs[i]
-#         best_stump=stumps[i]
+minMSE=float("inf")
+for i in range(noOfStumps):
+    if(minMSE>MSEs[i]):
+        minMSE=MSEs[i]
+        best_stump=stumps[i]
+        best_stump_no=i
 
-# # best_stump=stumps[accuracy_values.index(max(accuracy_values))]
-# # print(best_stump)
-# correct=0
-# for i in range(y_test.size):
-#     if(x_test[best_stump[0]][i]<=best_stump[1] and y_test[i]==best_stump[2]):
-#         correct+=100
-#     elif(x_test[best_stump[0]][i]>best_stump[1] and y_test[i]==best_stump[3]):
-#         correct+=100
-# print(f"accurracy on test set",correct/y_test.size)
+htest=[]
+for k in range(best_stump_no+1):
+    temp=[]
+    for i in range(y_test.size):
+        if(x_test[stumps[k][0]][i]<=stumps[k][1]):
+            temp.append(stumps[k][2])
+        else:
+            temp.append(stumps[k][3])
+    htest.append(temp)
+
+testMSE=0
+for j in range(y_test.size):
+    temp=y_test[j]
+    for i in range(best_stump_no+1):
+        temp-=(0.01)*htest[i][j]
+    testMSE+=temp**2
+
+testMSE/=y_val.size
+print("Final test MSE:",testMSE)
 
 stumps_range = range(1, noOfStumps+1)
 plt.figure(figsize=(10, 5))
 plt.plot(stumps_range, MSEs, marker='o')
-plt.title('Validation Set Accuracy vs Number of Stumps')
-plt.xlabel('Number of Stumps')
-plt.ylabel('Accuracy (%)')
-plt.xticks(stumps_range)
-plt.ylim(95, 100)
+plt.title('Validation Set MSE vs Number of Stumps')
+plt.xlabel('Number of Trees')
+plt.ylabel('MSE')
+plt.xticks(range(0, noOfStumps+1, 25))
+plt.ylim(0, 1) 
 plt.grid(True)
 plt.show()
